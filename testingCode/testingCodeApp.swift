b43bb532f846d54bc72f1bd21f6c8413af355cbb
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-10-10 09:59:20 +0200

    Initial Commit

---
+++ testingCode/testingCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingCodeApp.swift
+//  testingCode
+//
+//  Created by Omid Shojaeian Zanjani on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testingCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
